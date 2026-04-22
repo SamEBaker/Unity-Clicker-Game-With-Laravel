89b0e8c77b9ea5c59e41314ed09d749f570c2428
Samantha Esparza-Baker <esparzs1@my.erau.edu>
2026-04-21 22:27:28 -0700

    huh

--- PetDaDuck/routes/api.php
+++ PetDaDuck/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/leaderboard', [AuthController::class, 'leaderboard']);
+Route::get('/leaderboard', [AuthController::class, 'leaderboard']); 
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/update-score', [AuthController::class, 'saveScore']);
