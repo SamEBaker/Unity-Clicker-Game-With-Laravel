88baccc4fb715d8beb5cbaa8ea0ade70206961ec
Samantha Esparza-Baker <esparzs1@my.erau.edu>
2026-04-22 19:59:07 -0700

    oops

--- PetDaDuck/routes/api.php
+++ PetDaDuck/routes/api.php
@@ -4,12 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-// 1. Change 'auth:api' to 'auth:sanctum'
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-// 2. The Login route (No lock/middleware needed here)
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/leaderboard', [AuthController::class, 'leaderboard']); 
 
